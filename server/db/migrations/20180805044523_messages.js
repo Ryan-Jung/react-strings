0942685fa26a27078ccf88d174c22385f78f89c1
Ryan Jung <ryanjung93@gmail.com>
2018-08-05 16:33:41 -0700

    Added null constraint and default values to table

--- server/db/migrations/20180805044523_messages.js
+++ server/db/migrations/20180805044523_messages.js
@@ -1,12 +1,8 @@
+exports.up = knex =>
+  knex.schema.createTable('messages', table => {
+    table.increments();
+    table.string('message').notNullable();
+    table.timestamps(true, true);
+  });
 
-exports.up = function(knex, Promise) {
-  return knex.schema.createTable('messages', (table) => {
-      table.increments(),
-      table.string('message'),
-      table.timestamps()
-    })
-};
-
-exports.down = function(knex, Promise) {
-  return knex.schema.dropTable('messages')
-};
+exports.down = knex => knex.schema.dropTable('messages');
